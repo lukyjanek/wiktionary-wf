5608cb234b4777933ff4295dd08b4ca70a3d5428
root <l.kyjanek@gmail.com>
2018-02-02 20:33:22 +0100

    extraction all informations (in text format) for czech language

--- extraction.py
+++ extraction.py
@@ -1,8 +1,18 @@
 # !usr/bin/env python3
 # coding utf-8
 
-def cs(data):
-    return data
+import re
 
-def en(data):
+def cs(data): # czech language
+    infos = re.search(r'== čeština ==\n(.*\n)*', data) # entry contains information for czech
+    if (infos):
+        more = re.findall(r'((?<!=)== )', infos.group(0)) # number of languages in entry
+        if (len(more) > 1):
+            only = re.search(r'== čeština ==\n(.*\n)*?(== )', infos.group(0)) # extract czech only
+            return only.group(0)
+        else:
+            return infos.group(0)
+    return None
+
+def en(data): # english language
     return data
--- wiktionary.py
+++ wiktionary.py
@@ -17,11 +17,13 @@ with BZ2File(path) as xml_file:
         elif (element.tag[43:] == 'ns') and (element.text == '0'): # unlock saving lexeme if it is lexeme
             tkey = True
         elif (element.tag[43:] == 'text') and (tkey is True): # save informations about lexeme
-            lexeme_data[title] = eval(path[5:7])(element.text) # choose right function for extraction (how dangerous 'eval()' is?)
+            data = eval(path[5:7])(element.text) # choose right function for extraction (how dangerous 'eval()' is?)
+            if not (data is None):
+                lexeme_data[title] = data
             tkey = False
         element.clear()
 
-# with open('pokus.txt', mode='w', encoding='utf-8') as f:
-#     f.write('Number of lexemes: ' + str(len(lexeme_data)) + '\n\n')
-#     for i,j in lexeme_data.items():
-#         f.write(i + '\n' + j + '\n')
+with open('pokus.txt', mode='w', encoding='utf-8') as f:
+    f.write('Number of lexemes: ' + str(len(lexeme_data)) + '\n\n')
+    for i,j in lexeme_data.items():
+        f.write(i + '\n' + j + '\n')
